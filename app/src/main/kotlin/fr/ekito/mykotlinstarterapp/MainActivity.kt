de2bc27a35ba55e4965212d5b9e6f956fa01586a
Arnaud Giuliani <agiuliani@ekito.fr>
2016-06-14 17:14:05 +0200

    wip

--- app/src/main/kotlin/fr/ekito/mykotlinstarterapp/MainActivity.kt
+++ app/src/main/kotlin/fr/ekito/mykotlinstarterapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab!!.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab.setOnClickListener { view -> Snackbar.make(view, "Your action here !", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
